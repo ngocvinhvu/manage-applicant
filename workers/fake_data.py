d899686d9b7cc6a40b07799f5f8e0f94cdfb9b71
vinhvn <vungocvinh95@gmail.com>
2022-08-10 21:44:55 +0700

    Done scripts

--- workers/fake_data.py
+++ workers/fake_data.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from faker import Faker
 from datetime import datetime
 from config import config
@@ -9,36 +9,54 @@ import asyncio
 ENV = os.environ.get("ENV", "development")
 CONF = config[ENV]
 
+# Make a list of fake Applican's Infomations
+def make_list_dummy_applicants(num):
+    num_of_applicants = num
+    fake = Faker('de_DE')
+    applicants = []
+    for i in range(num_of_applicants):
+        # Create applicant date of birth 
+        d1 = datetime.strptime(f'1990/1/1', '%Y/%m/%d')
+        d2 = datetime.strptime(f'2010/1/1', '%Y/%m/%d')
+        dob = fake.date_between(d1, d2)
+        #create applicant's name
+        name = fake.name()
+        # Create email 
+        email = fake.ascii_email()
+        applicant = {}
+        applicant.update({"name": name, "email": email, "dob": str(dob).replace("-", "/"), "country": "VIETNAM"})
+        applicants.append(applicant)
+    return applicants
 
-num_of_applicants = 1000
 
-fake = Faker('de_DE')
+# Async and concurrency http post request 
+async def gather_with_concurrency(n, *tasks):
+    semaphore = asyncio.Semaphore(n)
+    async def sem_task(task):
+        async with semaphore:
+            return await task
+   
+    return await asyncio.gather(*(sem_task(task) for task in tasks))
 
-applicants = []
 
-for customers_id in range(num_of_applicants):
-    # Create applicant date of birth 
-    d1 = datetime.strptime(f'1990/1/1', '%Y/%m/%d')
-    d2 = datetime.strptime(f'2010/1/1', '%Y/%m/%d')
-    dob = fake.date_between(d1, d2)
-
-    #create applicant's name
-    name = fake.name()
-    
-    # Create email 
-    email = fake.ascii_email()
-
-    applicant = {}
-    applicant.update({"name": name, "email": email, "dob": str(dob).replace("-", "/"), "country": "VIETNAM"})
-    applicants.append(applicant)
+async def post_async(url, session, body):
+    count_success = 0
+    count_failed = 0
+    try:
+        async with session.post(url, json=body) as response:
+            text = await response.text()
+            count_success += 1
+    except Exception:
+        count_failed += 1
+    return count_success, count_failed
 
 
 async def main():
-    async with aiohttp.ClientSession() as session:
-        for applicant in applicants:
-            pokemon_url = f"{CONF.API_URL}/applicants"
-            async with session.post(url=pokemon_url, json=applicant) as resp:
-                applicant = await resp.json()
+    conn = aiohttp.TCPConnector(limit=None, ttl_dns_cache=300)
+    session = aiohttp.ClientSession(connector=conn)
+    url = CONF.API_URL + "/applicants"
+    conc_req = 10
+    _ = await gather_with_concurrency(conc_req, *[post_async(url, session, applicant) for applicant in make_list_dummy_applicants(1000)])
 
 
 def run():
